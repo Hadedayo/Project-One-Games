da810892405481183b74bc74efbcc3a139681fcc
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-15 14:10:11 +0100

    Added Dom ready function, user interaction using prompt and an array of words.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,11 @@
-$ (function(event){
+document.addEventListener("click", function (event) {
+
+  var userName = prompt("Hi there! What is your name?");
+  alert("Welcome to Word Scramble " + userName + "." + " This is an opportunity to improve your spelling skills and expand your vocabulary. So Enjoy!");
+
+  var count = 0;
+
+  var wordList = ["program","sunrise", "official", "flex", "cascading", "developer", "add", "multiply", "technology", "software", "function"]
 
 
 
@@ -13,10 +20,4 @@ $ (function(event){
 
 
 
-
-
-
-
-
-  
-});
+})
