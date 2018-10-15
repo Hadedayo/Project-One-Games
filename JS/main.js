2732c97311c47582c85359db42aadf26b0b44ed8
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-15 22:25:16 +0100

    Changed function name to "splitToCharacters". Characters are sorted alphabeticaally, joined to form a strinng and comma's are removed.

--- JS/main.js
+++ JS/main.js
@@ -5,25 +5,41 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 //When you click the button, it should display the instructions//
   function instructionBtn (){
-    document.getElementById("instructionText").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.";
+    document.getElementById("instructionText").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididun";
   }
-
-  //var count = 0;
 
   var wordListArray = ["program","enthrall", "official", "flex", "cascading", "developer", "syntax", "multiply", "technology", "software", "function", "sheet"]
 
   var randomWord = wordListArray[Math.floor(Math.random() * wordListArray.length)];
 
     //access a randomWord
-    //split the randomword into array (Arr) of letters
-    //then randomly
+    //splits the randomword into array (Arr) of letters
 
-  function scrambleWord(randomWord) {
+  function splitToCharacters(randomWord) {
     var characters = randomWord.split("");
     console.log(characters);
     return characters;
   }
-  scrambleWord (randomWord);
+  splitToCharacters (randomWord);
+
+  //.sort--sorts the items of an array alphabetically pr numerically
+  var scramble = splitToCharacters (randomWord);
+  scramble.sort()
+  console.log(scramble);
+
+  //.join --joins the elements of an array into a string + returns the string
+  var joinArray = scramble.join();
+  console.log(joinArray);
+
+//this function removes the commas in the string
+// replace the commas with what ever character you want, e.g I replaced the commas with space
+//g flag - generic expression that replaces all
+  function removeComma(joinArray) {
+    var rmv = joinArray.replace(/,/g , " ");
+    console.log(rmv);
+    return rmv;
+  }
+  removeComma(joinArray).toUpperCase;
 
 
 
