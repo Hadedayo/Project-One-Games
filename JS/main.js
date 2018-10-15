360af720299b488221ba80aec11112bfada2fdab
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-15 15:18:33 +0100

    Correctly added DOMCOntentLoaded to js file + random word generator

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,19 @@
-document.addEventListener("click", function (event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
   var userName = prompt("Hi there! What is your name?");
   alert("Welcome to Word Scramble " + userName + "." + " This is an opportunity to improve your spelling skills and expand your vocabulary. So Enjoy!");
 
-  var count = 0;
+//When you click the button, it should display the instructions//
+  function instructionBtn (){
+    document.getElementById("instructionText").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.";
+  }
 
-  var wordList = ["program","sunrise", "official", "flex", "cascading", "developer", "add", "multiply", "technology", "software", "function"]
+  //var count = 0;
+
+  var wordListArray = ["program","enthrall", "official", "flex", "cascading", "developer", "add", "multiply", "technology", "software", "function"]
+
+  var randomWord = wordListArray[Math.floor(Math.random() * wordListArray.length)];
+    console.log(randomWord);
 
 
 
