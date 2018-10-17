5921e7f488f6ef7f3bfcaf1fbd820e0474418c4f
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-17 09:45:27 +0100

    Added an eventlistener to tryClick button. Added a footer to html

--- JS/main.js
+++ JS/main.js
@@ -7,72 +7,62 @@ document.addEventListener("DOMContentLoaded", function (event) {
   function instructionBtn (){
     document.getElementById("instructionText").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididun";
   }
-
-  var wordListArray = [
-    "ARRAY",
-    "FLEX"  ,
-    "CASCADING" ,
-    "SHOW" ,
-    "SYNTAX",
-    "MULTIPLY" ,
-    "TECHNOLOGY"  ,
-    "SHEET"]
-
+  var wordListArray = ["ARRAY", "FLEX", "CASCADING", "SHOW" , "SYNTAX", "MULTIPLY" , "TECHNOLOGY" , "SHEET"]
   var randomWord = wordListArray[Math.floor(Math.random() * wordListArray.length)];
 
-    //access a randomWord
-    //splits the randomword into array (Arr) of letters
+  function randomWordGen() {
+    function splitToCharacters(randomWord) {
+      var characters = randomWord.split("");
+      console.log(characters);
+      return characters;
+    }
+    splitToCharacters (randomWord);
 
-  function splitToCharacters(randomWord) {
-    var characters = randomWord.split("");
-    console.log(characters);
-    return characters;
-  }
-  splitToCharacters (randomWord);
+    //.sort--sorts the items of an array alphabetically or numerically
+    var scramble = splitToCharacters (randomWord);
+    scramble.sort()
+    console.log(scramble);
 
-  //.sort--sorts the items of an array alphabetically or numerically
-  var scramble = splitToCharacters (randomWord);
-  scramble.sort()
-  console.log(scramble);
+    //.join --joins the elements of an array into a string + returns the string
+    var joinArray = scramble.join();
+    console.log(joinArray);
 
-  //.join --joins the elements of an array into a string + returns the string
-  var joinArray = scramble.join();
-  console.log(joinArray);
+    //this function removes the commas in the string
+    // replace the commas with what ever character you want, e.g I replaced the commas with space
+    //g flag - generic expression that replaces all
+    function removeComma(joinArray) {
+      var rmv = joinArray.replace(/,/g , "");
+      console.log(rmv);
+      return rmv;
+    }
+    removeComma(joinArray);
 
-  //this function removes the commas in the string
-  // replace the commas with what ever character you want, e.g I replaced the commas with space
-  //g flag - generic expression that replaces all
-  function removeComma(joinArray) {
-    var rmv = joinArray.replace(/,/g , "");
-    console.log(rmv);
-    return rmv;
-  }
-  removeComma(joinArray);
+    //displays the string without commas onto the webpage
+    var actualGameWords = removeComma(joinArray);
+    console.log("a="+actualGameWords.length);
+    var scrambledWordsOnScreen = document.getElementById("actualGameWords").innerHTML = actualGameWords;
+    }
+    randomWordGen();
 
-  //displays the string without commas onto the webpage
-  var actualGameWords = removeComma(joinArray);
-  console.log("a="+actualGameWords.length);
-  var scrambledWordsOnScreen = document.getElementById("actualGameWords").innerHTML = actualGameWords;
+    //created an empty array
+    //for loop--> makes sure that for the length of random scrambled word, I need to have the same number of underscores
+    //Replaces the empty array with underscores
+    var underscoreArray = [];
+    for (var i = 0; i < actualGameWords.length; i++) {
+     underscoreArray[i] = "_";
+     }
+    console.log(underscoreArray);
 
-  //created an empty array
-  //for loop--> makes sure that for the length of random scrambled word, I need to have the same number of underscores
-  //Replaces the empty array with underscores
-  var underscoreArray = [];
-  for (var i = 0; i < actualGameWords.length; i++) {
-   underscoreArray[i] = "_";
-   }
-  console.log(underscoreArray);
+    //.join --joins the elements of an array into a string + returns the string
+    //joins the [_,_,] into a string to remove commas
+    var joinArray = underscoreArray.join();
+    console.log(joinArray);
 
-  //.join --joins the elements of an array into a string + returns the string
-  //joins the [_,_,] into a string to remove commas
-  var joinArray = underscoreArray.join();
-  console.log(joinArray);
+    //joinArray contains a string of underscore with commas, now replace the commas with spaces
+    var addSpaceBtwUnderScores = joinArray.replace(/,/g , " ");
+    console.log(addSpaceBtwUnderScores);
 
-  //joinArray contains a string of underscore with commas, now replace the commas with spaces
-  var addSpaceBtwUnderScores = joinArray.replace(/,/g , " ");
-  console.log(addSpaceBtwUnderScores);
-
-  //the underscore will now be displayed on the webpage in place of the text in "User Types" of html file
+    //the underscore will now be displayed on the webpage in place of the text in "User Types" of html file
 
    var writeUnderscoreOnPage = document.getElementById("underscore").innerHTML = addSpaceBtwUnderScores;
 
@@ -80,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
    document.getElementById("testButtonId").addEventListener("click",function(){
      var userInput = document.getElementById("test").value;
      console.log(userInput);
-     if (userInput.toUpperCase()   === randomWord) {
+     if (userInput.toUpperCase() === randomWord) {
        console.log(userInput.toUpperCase );
        document.getElementById("correctOrIncorrect").innerHTML="You are correct!"
        }
@@ -88,11 +78,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
          document.getElementById("correctOrIncorrect").innerHTML="Sorry this is incorrect. The answer is: " + randomWord
        }
    });
-
-
-
-
-
-
-
+   document.getElementById("tryAgain").addEventListener("click", function (event) {
+      randomWordGen();
+      3
+   })
 })
