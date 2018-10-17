da79121cd9e436b88c2bad7a18b4afc966735f2f
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-17 16:35:01 +0100

    Added "onfocus" to <input> in my html-- replacs its current value with an empty string. Changed button name and id to submit button. Moved functions to the top of the page and calling them at the bottom

--- JS/main.js
+++ JS/main.js
@@ -8,12 +8,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var wordListArray = ["DISCOMBOBULATE", "FLEX", "CASCADING", "SHOW" , "SYNTAX", "MULTIPLY" , "TECHNOLOGY" , "SHEET", "SOFTWARE", "ENTHRALL"]
   var randomWord = wordListArray[Math.floor(Math.random() * wordListArray.length)];
 
-  function randomWordGen() {
-    function splitToCharacters(randomWord) {
-      var characters = randomWord.split("");
-      console.log(characters);
-      return characters;
+  function splitToCharacters(randomWord) {
+    var characters = randomWord.split("");
+    console.log(characters);
+    return characters;
+  }
+
+  function removeComma(joinArray) {
+    var rmv = joinArray.replace(/,/g , " ");
+    console.log(rmv);
+    return rmv;
+  }
+
+  // function removeComma(joinArray) {
+  //   var rmv = joinArray.replace(/,/g , " ");
+  //   console.log(rmv);
+  //   return rmv;
+  // }
+
+  function countdown() {
+    var count = 60;
+  setInterval(function(){
+    if (count > 0) {
+      count --;
+      console.log(count);
+      document.getElementById("counter").innerHTML = count;
+    };
+    if (count < 10) {
+      document.getElementById("counter").style.color = "red";
     }
+
+  },
+    1000);
+  }
+
+  function randomWordGen() {
+    randomWord = wordListArray[Math.floor(Math.random() * wordListArray.length)];
     splitToCharacters (randomWord);
 
     //.sort--sorts the items of an array alphabetically or numerically
@@ -28,65 +58,53 @@ document.addEventListener("DOMContentLoaded", function (event) {
     //this function removes the commas in the string
     // replace the commas with what ever character you want, e.g I replaced the commas with space
     //g flag - generic expression that replaces all
-    function removeComma(joinArray) {
-      var rmv = joinArray.replace(/,/g , " ");
-      console.log(rmv);
-      return rmv;
-    }
+
     removeComma(joinArray);
 
     //displays the string without commas onto the webpage
     var actualGameWords = removeComma(joinArray);
     console.log("a="+actualGameWords.length);
-    var scrambledWordsOnScreen = document.getElementById("actualGameWords").innerHTML = actualGameWords;
-    }
+    document.getElementById("actualGameWords").innerHTML = actualGameWords;
+  }
     //randomWordGen();
 
-    document.getElementById('Hints').addEventListener("click", function (event) {
-      var Hint = document.getElementById("hintInformation").innerHTML = "Give me Hintss";
-    })
+  document.getElementById('Hints').addEventListener("click", function (event) {
+    var Hint = document.getElementById("hintInformation").innerHTML = "Give me Hintss";
+  })
 
 
-
-    var count = 60;
-    setInterval(function(){
-      if (count > 0) {
-        count --;
-        console.log(count);
-        document.getElementById("counter").innerHTML = count;
-      };
-      if (count < 10) {
-        document.getElementById("counter").style.color = "red";
-      }
-    }, 1000);
+    // make a set interval and have it call a function every second
+    // the function is going to first check if count is greater than 0
+    // if it is greater than 0, decrease count by 1
+    // Then, set counter's html to count
+    // if it's not greater than 0, end the game
 
 
 
 
 
+ //User's input SECTION
+ document.getElementById("submitBtn").addEventListener("click",function(){
+   var userInput = document.getElementById("submit").value;
+   console.log(userInput);
+   if (userInput.toUpperCase() === randomWord) {
+     console.log(userInput.toUpperCase );
+     document.getElementById("correctOrIncorrect").innerHTML="You are correct!"
+     document.getElementById("correctOrIncorrect").style.backgroundColor = "green";
+     }
+     else {
+       document.getElementById("correctOrIncorrect").innerHTML="Incorrect. The answer was: " + randomWord
+       document.getElementById("correctOrIncorrect").style.backgroundColor = "red";
+     }
+    randomWordGen();
+    document.getElementById("userInput").value.innerHTML= " ";
+ })
 
+ document.getElementById("playGame").addEventListener("click",function(){
+   countdown();
+   randomWordGen();
 
-
-
-
-       //User's input SECTION
-       document.getElementById("testButtonId").addEventListener("click",function(){
-         var userInput = document.getElementById("test").value;
-         console.log(userInput);
-         if (userInput.toUpperCase() === randomWord) {
-           console.log(userInput.toUpperCase );
-           document.getElementById("correctOrIncorrect").innerHTML="You are correct!"
-           document.getElementById("correctOrIncorrect").style.backgroundColor = "green";
-           }
-           else {
-             document.getElementById("correctOrIncorrect").innerHTML="Incorrect. The answer is: " + randomWord
-             document.getElementById("correctOrIncorrect").style.backgroundColor = "red";
-           }
-       })
-
-
-
-    
+ })
 
 
 
